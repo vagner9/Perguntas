afb646a4c3944dee66c0aa805c839f21841c7ed5
vagner9 <143604125+vagner9@users.noreply.github.com>
2024-11-21 07:23:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é função?',
+    'É um bloco de código que realiza uma tarefa.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'É um espaço para guardar valores que podem mudar.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz vermelho em Inglês?',
+    'Vermelho em inglês é red.'
+)
